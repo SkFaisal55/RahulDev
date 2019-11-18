33afaab918263a37d70167259c47364318cd86b3
Sunit Mahyavanshi <smahyavanshi70@gmail.com>
2019-11-18 18:08:19 +0530

    RahulCommmit

---
+++ RahulDev/Models/AdminModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RahulDev.Models
+{
+    public class AdminModel
+    {
+        public int id { get; set; }
+        public string  Name { get; set; }
+       
+    }
+}
